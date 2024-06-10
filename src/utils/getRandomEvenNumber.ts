c9aca0a9b28d3e1a1d29fad0a73f617a110532c7
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-06-10 18:37:12 +0200

    chore: delete unused files

--- src/utils/getRandomEvenNumber.ts
+++
@@ -1,5 +0,0 @@
-export function getRandomEvenNumber() {
-  const supportedEvenNumbers = [2, 4, 6, 8]
-  const index = Math.floor(Math.random() * supportedEvenNumbers.length)
-  return supportedEvenNumbers[index]
-}
