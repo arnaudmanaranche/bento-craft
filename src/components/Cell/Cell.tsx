6dc99e4c27e869668c92e8ea7a427ecfb769e875
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-06-13 14:20:12 +0200

    fix: clean code

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,5 +1,6 @@
 import { useBentoStore } from '@/store'
 import clsx from 'clsx'
+import type { BaseSyntheticEvent } from 'react'
 import { useMemo } from 'react'
 
 export interface CellProps {
@@ -27,7 +28,7 @@ export function Cell({ rowIndex, columnIndex, size }: CellProps) {
     size[1] === 3 ? 'row-span-3' : '',
     size[1] === 2 ? 'row-span-2' : '',
     isSelected ? 'border-2 border-[#1D1D1D] dark:border-[#E9E9E9]' : '',
-    'rounded-lg min-h-20 flex items-center justify-center bg-[#E9E9E9] cursor-pointer text-[#1D1D1D]',
+    'rounded-lg min-h-40 flex items-center justify-center bg-[#E9E9E9] cursor-pointer text-[#1D1D1D]',
     'dark:text-white dark:bg-[#1D1D1D]'
   )
 
@@ -37,7 +38,7 @@ export function Cell({ rowIndex, columnIndex, size }: CellProps) {
       className={className}
       data-row-index={rowIndex}
       data-col-index={columnIndex}
-      onClick={(event) => {
+      onClick={(event: BaseSyntheticEvent) => {
         if (isSelected) {
           const index = selectedCells.findIndex(
             (cell) => cell[0] === rowIndex && cell[1] === columnIndex
@@ -47,16 +48,14 @@ export function Cell({ rowIndex, columnIndex, size }: CellProps) {
             unSetSelectCell(index)
           }
         } else {
-          const element = event.target as HTMLInputElement
-
           setSelectCell([
             parseInt(
-              element.attributes.getNamedItem('data-row-index')
+              event.target.attributes.getNamedItem('data-row-index')
                 ?.value as string,
               10
             ),
             parseInt(
-              element.attributes.getNamedItem('data-col-index')
+              event.target.attributes.getNamedItem('data-col-index')
                 ?.value as string,
               10
             ),
@@ -64,13 +63,7 @@ export function Cell({ rowIndex, columnIndex, size }: CellProps) {
         }
       }}
     >
-      <span
-        data-row-index={rowIndex}
-        data-col-index={columnIndex}
-        data-selected={isSelected}
-      >
-        Cell {[rowIndex, columnIndex].toString()}
-      </span>
+      <span>Cell {[rowIndex, columnIndex].toString()}</span>
     </div>
   )
 }
